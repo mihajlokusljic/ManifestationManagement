314266a88b2e82052020bf8bbd36977ddc85332a
Mihajlo <mihajlokusljic97@gmail.com>
2019-06-01 01:53:47 +0200

    Prikaz liste manifestacija za drag na mapi

--- ManifestationManagementApp/ManifestationManagementApp/view/MapView.xaml.cs
+++ ManifestationManagementApp/ManifestationManagementApp/view/MapView.xaml.cs
@@ -12,17 +12,46 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using ManifestationManagementApp.model;
 
 namespace ManifestationManagementApp.view
 {
     /// <summary>
     /// Interaction logic for MapView.xaml
     /// </summary>
-    public partial class MapView : Page
+    public partial class MapView : Page, INotifyPropertyChanged
     {
+        private ObservableCollection<Manifestation> manifestations;
+        public ObservableCollection<Manifestation> Manifestations
+        {
+            get { return manifestations; }
+            set
+            {
+                if (value != manifestations)
+                {
+                    manifestations = value;
+                    OnPropertyChanged("Manifestations");
+                }
+            }
+        }
+
         public MapView()
         {
             InitializeComponent();
+
+            DataContext = this;
+            Manifestations = Repository.GetInstance().Manifestations;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
         }
     }
 }
